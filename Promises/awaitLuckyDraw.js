97f2f71d74f93c5d78dfbc59dd295677979508f0
Simone <108352654+smazza98@users.noreply.github.com>
2023-03-24 11:47:31 +0100

    Create awaitLuckyDraw.js

---
+++ Promises/awaitLuckyDraw.js
@@ -0,0 +1,14 @@
+async function getResults() {
+  const players = ['Tina', 'Jorge', 'Julien'];
+
+  for (const player of players) {
+    try {
+      const result = await luckyDraw(player);
+      console.log(result);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
+
+getResults();
